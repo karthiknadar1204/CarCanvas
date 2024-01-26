9b91b9788c7f5f2e7db72a5b1d894c30428b795f
karthik <karthiknadar1204@gmail.com>
2024-01-26 15:47:24 +0530

    updating the final build

---
+++ carcanvas/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Navbar from "@components/Navbar";
+import Footer from "@components/Footer";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "CarCanvas",
+  description: "Discover the best cars in the world",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <Navbar/>
+        {children}
+        <Footer/>
+      </body>
+    </html>
+  );
+}
